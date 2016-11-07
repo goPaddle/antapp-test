f62050cc3c4f9f0b440b9cd22467f3f39dae7395
goPaddle <testgopaddle@gmail.com>
2016-11-07 14:55:36 +0530

    Update RunTest.java

--- src/com/bluemeric/test/RunTest.java
+++ src/com/bluemeric/test/RunTest.java
@@ -20,9 +20,7 @@ import com.bluemeric.common.TestApp;
 import com.bluemeric.common.Util;
 
 public class RunTest  implements ITestListener {
-	//static String projectHome = System.getProperty("PROJECT_HOME") + "/";
-	//static String endpoint = System.getProperty("APP_ENDPOINT"); 
-
+	
 	static String projectHome = System.getProperty("user.dir")+"/";
 	static String endpoint = "localhost:8080";
 	
